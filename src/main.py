e6618b45f27568fb00da2f6bf4e319eda7194741
Mernom <120170520+Mernomoff@users.noreply.github.com>
2025-04-16 23:02:16 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,8 @@
 import uvicorn
 from fastapi import FastAPI
 
-from routers import router
 from settings import settings
+from routers import router
 
 app = FastAPI(debug=False)
 app.include_router(router=router)
